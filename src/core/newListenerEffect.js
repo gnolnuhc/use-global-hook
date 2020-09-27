d732bd0af874ea635b094bb2044b791e06249972
Chunlong Li <chunzibox@gmail.com>
2020-09-27 11:06:27 -0700

    Set correct initial state

--- src/core/newListenerEffect.js
+++ src/core/newListenerEffect.js
@@ -1,15 +1,15 @@
 import { cleanUpListener } from "./cleanUpListener";
 
 export const newListenerEffect = (store, mapState, originalHook) => () => {
-  const newListener = { oldState: {} };
+  const newListener = { oldState: mapState ? mapState(store.state) : store.state };
   newListener.run = mapState
     ? (newState) => {
-        const mappedState = mapState(newState);
-        if (mappedState !== newListener.oldState) {
-          newListener.oldState = mappedState;
-          originalHook(mappedState);
-        }
+      const mappedState = mapState(newState);
+      if (mappedState !== newListener.oldState) {
+        newListener.oldState = mappedState;
+        originalHook(mappedState);
       }
+    }
     : originalHook;
   store.listeners.push(newListener);
   return cleanUpListener(store, newListener);
